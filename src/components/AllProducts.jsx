bb0aaaabb8cd2ab455bfb048b3d60d2e8b0568bd
Ashim Gautam <computer77356@gmail.com>
2024-11-19 22:26:53 +0545

    ui height error is fixed

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -6,17 +6,17 @@ import { Link } from 'react-router-dom'
 const AllProducts = () => {
     const allProducts = useRecoilValue(productsAtom())
   return (
-    <div className='border border-red-700 grid grid-cols-4 w-full pr-4 pl-4 font-primary'>
+    <div className='border border-red-700 grid grid-cols-4 grid-rows-5 w-full gap-4 pr-4 pl-4 font-primary'>
         {
             allProducts.map(product=>{
                 return(
-                    <div className='border  border-blue-700 w-full flex flex-col justify-center items-center group cursor-pointer' key={product.id}>
-                        <div className='w-[80%] h-[80%] border border-black '>
+                    <div className='border py-2 col-span-1 row-span-1  border-blue-700 w-full h-full flex  flex-col justify-between items-center group cursor-pointer' key={product.id}>
+                        <div className='relative w-[80%] h-[60%]  '>
                             <Link to={`/${product.id}`}>
-                            <img src={product.image} alt={product.title} className='w-full h-full object-contain object-center border border-green-800'/>
+                            <img src={product.image} alt={product.title} className='w-full h-full object-contain object-center'/>
                             </Link>
                         </div>
-                        <div className="hidden group-hover:flex flex-col justify-center items-center  relative">
+                        <div className="hidden border border-red-700 h-[30%] group-hover:flex group-hover:flex-col w-[80%] justify-center items-center relative transition-all duration-300 ">
                             <p>{product.title}</p>
                             <p>{product.price}$</p>
                             <Link className='underline' to={`/${product.id}`} >Shop Now</Link>
